3149fdf8d7727428c162dde5ed9549d40806382c
BeatUp Company <atraban.beatup@gmail.com>
2016-11-20 23:21:30 +0330

    Add files via upload

---
+++ example-project/Assets/ExampleConvert.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleConvert : MonoBehaviour {
+	public AudioClip clip;
+	// Use this for initialization
+	void Start () {
+		
+
+		EncodeMP3.convert (clip, Application.dataPath + "/convertedMp3.mp3", 128);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
